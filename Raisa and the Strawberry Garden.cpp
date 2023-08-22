617f4a457ee711d615b2db2f13e7329c27f446d8
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-08-23 01:10:35 +0600

    Add files via upload

---
+++ Raisa and the Strawberry Garden.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    int totalStrawberries = 4 * X; // Each tree has X strawberries
+    cout << "Hello Zarif! Have " << totalStrawberries << " strawberries." << endl;
+
+    return 0;
+}
